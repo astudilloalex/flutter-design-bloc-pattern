9daec839eda82d34f45fca343b37d6da769713bf
Alex Astudillo <jlara@baustro.fin.ec>
2024-07-26 16:57:44 -0500

    feat: Add security code screen

---
+++ lib/presentation/screens/security_code/security_code_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class SecurityCodeScreen extends StatelessWidget {
+  const SecurityCodeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          AppLocalizations.of(context)!.securityCode,
+        ),
+        centerTitle: false,
+      ),
+    );
+  }
+}
